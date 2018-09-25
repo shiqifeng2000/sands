f6c121af56d4824758395490216983e8f7ccf575
shiqifeng <303772431@qq.com>
2018-09-25 16:06:33 +0800

    123

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @author robin
+ * @file index
+ * @date 2018-09-25 15:33
+ */
+
+const hello = function(key){
+  console.log('Hello World!');
+};
+const world = function(key){
+  console.log('Hello World 1232131!');
+};
+export const hello = hello;
+export default world
